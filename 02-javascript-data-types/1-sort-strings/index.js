7144c9eee27c1a00c30cdd27c22f734700d8fc2f
Archibald85 <ArtF85@yandex.ru>
2022-11-29 22:55:53 +0300

    Реализованы задачи: 1)Сортировка строк 2)Сборка объекта по заданным полям 3)Сборка объекта по полям не переданным в вызове

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const directions = {
+    'asc' : 1,
+    'desc' : -1,
+  };
+  const currentDirection = directions[param];
+  return [...arr].sort((a,b) => {
+    return currentDirection * a.localeCompare(b, ['ru', 'en'],{caseFirst : 'upper'});
+  });
 }
