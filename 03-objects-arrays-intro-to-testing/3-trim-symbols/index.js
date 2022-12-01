58bfc5fd89080da8ade02537f8c6e8f2bf4017b9
Archibald85 <ArtF85@yandex.ru>
2022-12-01 18:46:49 +0300

    Реализованы задачи: 1)createGetter 2)invertObj 3)trimSymbols 4)uniq

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string.length || size === 0) {
+    return '';
+  }
+  if (!size) {
+    return string;
+  }
+  const arrStr = string.split('');
+  let resultArr = [];
+  let currentArr = [arrStr[0]];
 
+  for (let i = 1; i <= arrStr.length; i++) {
+    if (currentArr[0] === arrStr[i]) {
+      if (currentArr.length < size) {
+        currentArr.push(arrStr[i]);
+      }
+    } else {
+      resultArr = [...resultArr, ...currentArr];
+      currentArr = [];
+      currentArr.push(arrStr[i]);
+    }
+  }
+  return resultArr.join('');
 }
