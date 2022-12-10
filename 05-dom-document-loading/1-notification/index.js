26d59ae03214fc2d95f845646ab6d24259c05c90
Archibald85 <ArtF85@yandex.ru>
2022-12-10 12:50:13 +0300

    1)Реализован NotificationComponent 2)Реализован SortableTable

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,66 @@
 export default class NotificationMessage {
+  static  notificationInstance;
+  constructor(
+    message = '',
+    {
+      duration = 0,
+      type = ''} = {}
+  ) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = document.getElementsByClassName('notification')[0] || null;
+    if (this.element) {
+      this.destroy();
+    }
+    this.render();
+  }
 
+  render() {
+    const element = document.createElement("div");
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${Math.floor(this.duration / 1000)}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  show(container = document.body) {
+    if (NotificationMessage.notificationInstance) {
+      NotificationMessage.notificationInstance.remove();
+    }
+
+    NotificationMessage.notificationInstance = this.element;
+    container.append(this.element);
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.timer = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    clearTimeout(this.timer);
+  }
 }
